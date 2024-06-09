1311503c557d2c9ee48308d1f746f69b4cc1cd07
RadMaxim <radchuk.maksime@gmail.com>
2024-06-09 12:28:01 +0300

    efw

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const { fs, http, path, JSDOM} = test;
 http.createServer((rej, res)=>{
     if (rej.url=='/') {
         
-        let index = fs.readFileSync(path.join("./../","clients","index.html"),{encoding:"utf-8"})
+        // let index = fs.readFileSync(path.join("./../","clients","index.html"),{encoding:"utf-8"})
         res.end("Server work")
     }
     // else if (rej.url =='/index.css') {
