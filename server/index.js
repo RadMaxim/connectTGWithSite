f1b9711fa81490576be6da7871525cc308551b9b
RadMaxim <radchuk.maksime@gmail.com>
2024-06-09 12:49:17 +0300

    api

--- server/index.js
+++ server/index.js
@@ -1,14 +1,9 @@
-import test from "./testServer.js"
-const { fs, http, path, JSDOM} = test;
-
-http.createServer((rej, res)=>{
-    if (rej.url=='/') {
-        
-        // let index = fs.readFileSync(path.join("./../","clients","index.html"),{encoding:"utf-8"})
-        res.end("Server work")
-    }
-    // else if (rej.url =='/index.css') {
-    //     let css = fs.readFileSync(path.join( "./../","client","index.css"),{encoding:"utf-8"})
-    //     res.end("css")
-    // }
-}).listen(3000)
+const express = require('express');
+const app = express();
+app.get("/",(req,res)=>{
+    res.send("Express on Vercel")
+})
+app.listen(3000,()=>{
+    console.log("Server is ready")
+})
+module.exports = app;
