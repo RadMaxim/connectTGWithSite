a7324c2cc39d472d2825376333be043ce5806b58
RadMaxim <radchuk.maksime@gmail.com>
2024-06-13 20:19:21 +0300

    replace

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-import test from "./testServer.js"
-const { fs, http, path, JSDOM} = test;
+const exp = require('express');
+const app = exp();
 
-http.createServer((rej, res)=>{
-    if (rej.url=='/') {
-        
-        // let index = fs.readFileSync(path.join("./../","clients","index.html"),{encoding:"utf-8"})
-        res.end("Server work")
-    }
-    // else if (rej.url =='/index.css') {
-    //     let css = fs.readFileSync(path.join( "./../","client","index.css"),{encoding:"utf-8"})
-    //     res.end("css")
-    // }
-}).listen(3000)
+const PORT = process.env.PORT || 8080;
+
+app.use(exp.json());
+
+app.get('/', (req, res) => {
+    res.send('products api running new deploy');
+});
+
+app.get('/ping', (req, res) => {
+    res.send('PONG')
+});
+// /products
+
+// /users
+
+
+app.listen(8080, () => {
+    console.log('Server is listenin on PORT :' + PORT);
+})
