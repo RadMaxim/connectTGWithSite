fb7c31a7d3a43416d867e12970751493f58aa1ff
RadMaxim <radchuk.maksime@gmail.com>
2024-06-13 22:51:56 +0300

    Maxim

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8080;
 app.use(exp.json());
 
 app.get('/', (req, res) => {
-    res.send('products api running new deploy');
+    res.send('products api running new deploy with Maxim');
 });
 
 app.get('/ping', (req, res) => {
